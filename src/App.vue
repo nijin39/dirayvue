5f9cbc453d1fc4ac490d4233acd19a6af0f54b2c
KIMJONGIL <nijin39@gmail.com>
2017-11-18 06:51:04 +0000

    init project

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <el-menu :default-active="activeIndex" :router="true" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+  <el-menu-item index="1" :route="{name:'HelloWorld'}">Processing Center</el-menu-item>
+  <el-submenu index="2">
+    <template slot="title">Workspace</template>
+<el-menu-item index="2-1">item one</el-menu-item>
+<el-menu-item index="2-2">item two</el-menu-item>
+<el-menu-item index="2-3">item three</el-menu-item>
+</el-submenu>
+<el-menu-item index="3" :route='{path:"/test"}' >Orders</el-menu-item>
+</el-menu>
+
+<!--<img src="./assets/logo.png">-->
+<router-view/>
+</div>
+</template>
+
+<script>
+  export default {
+    name: 'app',
+    data () {
+      return {
+        activeIndex: '1',
+        activeIndex2: '1'
+      }
+    },
+    methods: {
+      handleSelect (key, keyPath) {
+        console.log(key, keyPath)
+      }
+    }
+  }
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
+</style>
---
+++ src/components/Test.vue
@@ -0,0 +1,17 @@
+<template>
+    <h3>HIHIHI</h3>
+</template>
+
+<script>
+export default {
+  name: 'Test',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+<style type="text/css">
+    
+</style>
